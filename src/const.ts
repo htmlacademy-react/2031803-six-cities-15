a8f2688931c9808a0a17b07b1d662a538324ac98
Elina <elina50505051@yandex.ru>
2024-02-25 19:21:05 +0300

    feat: add routing

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,16 @@
 export const Settings = {
   OffersCount: 10
 };
+
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id',
+  Unknown = '*'
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+}
---
+++ src/pages/error404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {AppRoute} from "../const.ts";
+
+const Error404Page = () : React.JSX.Element => (
+  <>
+    <h1>Ошибка 404. Страница не существует.</h1>
+    <Link to={AppRoute.Main}>Главная страница</Link>
+  </>
+);
+
+export default Error404Page;
